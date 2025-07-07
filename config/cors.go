47f933f9a4225eb339d0c17ed247c836c080f9ab
Aqila Zafira <hartaticpg@gmail.com>
2025-07-07 17:35:53 +0700

    url fe

--- config/cors.go
+++ config/cors.go
@@ -6,6 +6,7 @@ var allowedOrigins = []string{
 	"http://localhost:5174/",
 	"http://localhost:5173/",
 	"http://127.0.0.1:8088/",
+	"https://frontend-praktikum-wheat.vercel.app",
 }
 
 func GetAllwedOrigins() []string {
